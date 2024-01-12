1cd72f09b811a8bd58cb1baea8389e70e6e9bec9
Lucas Santos <lucassantosrocha889@gmail.com>
2024-01-12 15:49:14 -0300

    Desafio finalizado!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,23 +2,39 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; }
+        private string Modelo { get; }
+        private string IMEI { get; }
+        private int Memoria { get; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
+
+        // Métodos para obter informações
+        public string ObterModelo() => Modelo;
+        public string ObterIMEI() => IMEI;
+        public int ObterMemoria() => Memoria;
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando para...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação de...");
+        }
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Número: {Numero}");
+            Console.WriteLine($"Modelo: {ObterModelo()}");
+            Console.WriteLine($"IMEI: {ObterIMEI()}");
+            Console.WriteLine($"Memória: {ObterMemoria()}GB");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
